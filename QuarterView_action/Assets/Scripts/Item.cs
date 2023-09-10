043db98e19ab2b75b17903ec4b68741625716b4b
wyllowyllo <105272901+wyllowyllo@users.noreply.github.com>
2023-09-11 01:02:11 +0900

    Item visual effects,and etc
    -Lighting effect
    -Particle effect
    -basic logic

---
+++ QuarterView_action/Assets/Scripts/Item.cs
@@ -0,0 +1,18 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Item : MonoBehaviour
+{
+
+    public enum Type {Ammo,Coin,Grenade,Heart,Weapon };
+    public Type type;
+    public int value;
+
+
+    // Update is called once per frame
+    void Update()
+    {
+        transform.Rotate(Vector3.up * 20 * Time.deltaTime);
+    }
+}
